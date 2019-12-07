f197b7bfcaaac81eb478cdfa4934a96c2474387b
Carlos Hanson <chanson@ttsd.k12.or.us>
2019-12-07 03:15:40 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # html-attributes
 All non-experimental attributes from the "HTML attribute reference" on MDN Web Docs.
+
+
+Install Pipenv: https://pipenv.kennethreitz.org/en/latest/
+
+    git clone https://github.com/carloshanson/html-attributes.git
+    cd html-attributes
+    pipenv install
+    scrapy runspider attributes_spider.py -o attributes.json
+
---
+++ attributes_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class AttrbitutesSpider(scrapy.Spider):
+    name = "attributes"
+    start_urls = ['https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes']
+
+    def parse(self, response):
+        for row in response.xpath('//article/table/tbody/tr'):
+            experimental = row.xpath('td[1]/span/i/@class').get() == 'icon-beaker'
+            if experimental:
+                continue
+            attributes = set()
+            attributes.add(row.xpath('td[1]/code/a[contains(@href, "Attributes")]/text()').get())
+            attributes.add(row.xpath('td[1]/code/a[contains(@href, "Global_attributes")]/text()').get())
+            attributes.add(row.xpath('td[1]/code/a/text()').get())
+            attributes.add(row.xpath('td[1]/a/code/text()').get())
+            attributes.add(row.xpath('td[1]/code/text()').get())
+            attributes.add(row.xpath('td[1]/text()').get())
+            attributes.remove(None)
+
+            tags = row.xpath('td[2]/a[contains(@href, "Element")]/code/text()').getall()
+            global_attribute = row.xpath('td[2]/a[contains(@href, "Global_attributes")]/text()').get() == "Global attribute"
+            yield {
+                    'name': attributes.pop(),
+                    'tags': [tag.replace('<','').replace('>','') for tag in tags],
+                    'is_global': global_attribute
+                    }
+
